1818b085c09a1e4549a3c861f309e741f10b82d4
brightlightkim <taeyangk95@naver.com>
2021-12-08 12:23:24 +0900

    add

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-fs.readFile(__dirname + '/cities.dat.txt', function (err, data) {
-  if (err) throw err;
-  var cities = data.toString().split("\n");
-  for (var i = 0; i < cities.length; i++) {
-    console.log(cities[i]);
-  }
-});
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -15,6 +8,13 @@ router.get('/', function (req, res, next) {
 });
 router.get('/getcity', function (req, res, next) {
   console.log("In getcity route");
+  fs.readFile(__dirname + '/cities.dat.txt', function (err, data) {
+    if (err) throw err;
+    var cities = data.toString().split("\n");
+    for (var i = 0; i < cities.length; i++) {
+      console.log(cities[i]);
+    }
+  });
 });
 
 module.exports = router;
