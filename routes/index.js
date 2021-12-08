a92f35bdbe02e4772e87ca2be3b223875e44eb08
brightlightkim <taeyangk95@naver.com>
2021-12-08 11:50:33 +0900

    changed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+fs.readFile(__dirname + '/cities.dat.txt', function (err, data) {
+  if (err) throw err;
+  var cities = data.toString().split("\n");
+  for (var i = 0; i < cities.length; i++) {
+    console.log(cities[i]);
+  }
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.sendFile('index.html', { root: 'public' });
+});
+router.get('/getcity', function (req, res, next) {
+  console.log("In getcity route");
 });
 
 module.exports = router;
